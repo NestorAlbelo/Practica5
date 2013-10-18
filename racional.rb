c9ed7db80a233ccc5514f58c3d5090736a290953
Nestor Albelo <alu0100598322@ull.edu.es>
2013-10-18 12:47:15 +0100

    Nestor Albelo Jorge

--- racional.rb
+++ racional.rb
@@ -1,6 +1,41 @@
 # Implementar en este fichero la clase para crear objetos racionales
 
-require "./gcd.rb"
+require './gcd.rb'
 
 class Fraccion
+
+	#Definicion de los gets
+	attr_reader :numerador, :denominador
+	
+	def initialize(numerator, denominator)
+		@numerador = numerator
+		@denominador = denominator
+	end
+	def to_s
+		"(#{numerador}/#{denominador})"
+	end
+	def + (frac)
+		a=(numerador*frac.denominador + frac.numerador*denominador)
+		b=(denominador*frac.denominador)
+		Fraccion.new((a/gcd(a,b)),(b/gcd(a,b)))
+	end
+	def - (frac)
+		a=(numerador*frac.denominador - frac.numerador*denominador)
+		b=(denominador*frac.denominador)
+		Fraccion.new((a/gcd(a,b)),(b/gcd(a,b)))
+	end
+	def * (frac)
+		a=(numerador*frac.numerador)
+		b=(denominador*frac.denominador)
+		Fraccion.new((a/gcd(a,b)),(b/gcd(a,b)))
+	end
+	def / (frac)
+		a=(numerador*frac.denominador)
+		b=(denominador*frac.numerador)
+		Fraccion.new((a/gcd(a,b)),(b/gcd(a,b)))
+	end
+	
+	
 end
+
+
--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,19 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+	def test_simple
+		assert_equal "(12/8)", Fraccion.new(12,8).to_s
+		assert_equal "(3/1)",(Fraccion.new(12,8)+Fraccion.new(6,4)).to_s
+		assert_equal "(0/1)",(Fraccion.new(12,8)-Fraccion.new(6,4)).to_s
+		assert_equal "(3/1)",(Fraccion.new(10,5)*Fraccion.new(6,4)).to_s
+		assert_equal "(1/1)",(Fraccion.new(12,8)/Fraccion.new(6,4)).to_s
+
+		assert_not_equal "(20/5)", Fraccion.new(12,8).to_s
+		assert_not_equal "(8/5)",(Fraccion.new(12,8)+Fraccion.new(6,4)).to_s
+		assert_not_equal "(4/5)",(Fraccion.new(12,8)-Fraccion.new(6,4)).to_s
+		assert_not_equal "(6/0)",(Fraccion.new(10,5)*Fraccion.new(6,4)).to_s
+		assert_not_equal "(7/12)",(Fraccion.new(12,8)/Fraccion.new(6,4)).to_s
+
+	end
 
 end
